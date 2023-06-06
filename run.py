9574cd7e4e270fcff5e22f5c137387112cedbe64
MickKav <>
2023-06-06 18:37:08 +0000

    Create while loop with if statement to randomise and verify location for ships

--- run.py
+++ run.py
@@ -1,15 +1,17 @@
 from random import randint
 
-board = []
 
 # Function to create an empty board
 
+
 def create_board():
+    board = []
     for _ in range(6):
         board.append('O')
     return board
 
 # Function to print the board
+
 
 def print_board(board):
     for row in board:
@@ -17,8 +19,15 @@ def print_board(board):
 
 # Function to place the ships
 
-def place_ships():
-    pass
+
+def place_ships(board):
+    ships = 0
+    while ships < 5:
+        ship_row = randint(0, 5)
+        ship_col = randint(0, 5)
+        if board[ship_row][ship_col] != 'S':
+            board[ship_row][ship_col] = 'S'
+            ships += 1
 
 # Function that initializes and starts the game
 
