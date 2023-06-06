c32eb953213d0d2c6efc97204d93da6fc35f35ea
MickKav <>
2023-06-06 18:21:10 +0000

    initial commit

--- run.py
+++ run.py
@@ -1,3 +1,22 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+# Function to create an empty board
+
+def create_board():
+    pass
+
+# Function to print the board
+
+def print_board():
+    pass
+
+# Function to place the ships
+
+def place_ships():
+    pass
+
+# Function that initializes and starts the game
+
+def play_game():
+    pass
+
